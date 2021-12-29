09b113e6866da8a6dcb2ccf8c2886dd9eb40b83e
larry <492178939@qq.com>
2021-12-29 18:03:16 +0800

    购物车统计数量和价格

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -3,19 +3,22 @@
     <nav-bar class="nav-bar">
       <div slot="center">购物车({{ length }})</div>
     </nav-bar>
-    <cart-list></cart-list>
+    <cart-list/>
+    <cart-bottom-bar/>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import CartList from "./childComps/CartList";
+import CartBottomBar from "./childComps/CartBottomBar";
 import {mapGetters} from 'vuex'
 export default {
   name: "Cart",
   components: {
     NavBar,
-    CartList
+    CartList,
+    CartBottomBar
   },
   computed:{
     //可以有两种语法
@@ -30,6 +33,9 @@ export default {
 </script>
 
 <style scoped>
+.cart{
+height: 100vh;
+}
 .nav-bar{
   background: var(--color-tint);
   color: white;
